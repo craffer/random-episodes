92f047103824f6dfe2c1a2d79d338080dfab7680
Conor Rafferty <craffer@umich.edu>
2019-02-08 16:39:09 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+Random episode generator.
+
+Conor Rafferty <craffer@umich.edu>
+"""
+
+from setuptools import setup
+
+setup(
+    name='randepisode',
+    version='0.1.0',
+    packages=['randepisode'],
+    include_package_data=True,
+    install_requires=[
+        'tmdbsimple'
+    ],
+    entry_points={
+        'console_scripts': [
+            'randepisode = randepisode.__main__:main'
+        ]
+    },
+)
