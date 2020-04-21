0d51c1faf967770a956598a5f1e3d4bb049994dc
Conor Rafferty <craffer@umich.edu>
2020-04-21 19:00:21 -0400

    Add script to run the flask app

--- randepisode/__init__.py
+++ randepisode/__init__.py
@@ -7,6 +7,11 @@ app = flask.Flask(__name__)  # pylint: disable=invalid-name
 # Read settings from config module (randepisode/config.py)
 app.config.from_object("randepisode.config")
 
+# Overlay settings read from file specified by environment variable. This is
+# useful for using different on development and production machines.
+# Reference: http://flask.pocoo.org/docs/0.12/config/
+app.config.from_envvar("INSTA485_SETTINGS", silent=True)
+
 # tell our app about our API and views files
 import insta485.api  # noqa: E402  pylint: disable=wrong-import-position
 import insta485.views  # noqa: E402  pylint: disable=wrong-import-position
