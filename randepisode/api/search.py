10f6825e97ba7ac9822a0618e630486c1550c4c6
Conor Rafferty <craffer@umich.edu>
2020-04-21 19:59:56 -0400

    Clarify response for search endpoint

--- randepisode/api/search.py
+++ randepisode/api/search.py
@@ -1,5 +1,6 @@
 """API endpoints for searching for TV shows."""
 import flask
+import requests
 import randepisode
 import tmdbsimple as tmdb
 
@@ -10,5 +11,12 @@ def query_tmdb_search(search_term=""):
     context = {}
     user_query = search_term if search_term else flask.request.args.get("q")
     search_obj = tmdb.Search()
-    context["results"] = search_obj.tv(query=user_query)
+    try:
+        results = search_obj.tv(query=user_query)
+    except requests.HTTPError as e:
+        # server error, I screwed something up
+        print(e)
+        return context, 500
+
+    context["results"] = results["results"]
     return flask.jsonify(**context)
