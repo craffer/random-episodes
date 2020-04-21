248cb445f1ca74767f94e70a1723d118ad0d35dd
Conor Rafferty <craffer@umich.edu>
2020-04-21 19:47:26 -0400

    Add endpoint to route search to TMDB

--- randepisode/api/search.py
+++ randepisode/api/search.py
@@ -1,1 +1,14 @@
 """API endpoints for generating a random episode of selected TV shows."""
+import flask
+import randepisode
+import tmdbsimple as tmdb
+
+
+@randepisode.app.route("/api/v1/search")
+def query_tmdb_search(search_term=""):
+    """Query TMDB for search results."""
+    context = {}
+    user_query = search_term if search_term else flask.request.args.get("q")
+    search_obj = tmdb.Search()
+    context["results"] = search_obj.tv(query=user_query)
+    return flask.jsonify(**context)
