f0e609fc7d2b2f258a0a6c4c5c989b671e0e7011
Conor Rafferty <craffer@umich.edu>
2020-04-21 17:27:51 -0400

    Add black formatting

--- randepisode/__main__.py
+++ randepisode/__main__.py
@@ -9,20 +9,20 @@ tmdb.API_KEY = os.environ["TMDB_KEY"]
 def get_random_episode(shows):
     """Return ID of a random episode given a list of TV shows."""
     random_show = random.choice(shows)
-    num_seasons = random_show['last_episode_to_air']['season_number']
+    num_seasons = random_show["last_episode_to_air"]["season_number"]
     random_season_num = random.randint(1, num_seasons)
-    season = tmdb.TV_Seasons(random_show['id'], random_season_num)
-    return random_show, random.choice(season.info()['episodes'])
+    season = tmdb.TV_Seasons(random_show["id"], random_season_num)
+    return random_show, random.choice(season.info()["episodes"])
 
 
 def main():
     """Random episode generator."""
     # gets the list of desired shows from the user
     show_names = []
-    print('When finished, hit ENTER.')
+    print("When finished, hit ENTER.")
     while True:
-        show_name = input('Enter a TV show: ')
-        if show_name == '':
+        show_name = input("Enter a TV show: ")
+        if show_name == "":
             break
         show_names.append(show_name)
 
@@ -32,8 +32,8 @@ def main():
     search = tmdb.Search()
     for name in show_names:
         response = search.tv(query=name)
-        show = response['results'][0]
-        tv_obj = tmdb.TV(id=show['id'])
+        show = response["results"][0]
+        tv_obj = tmdb.TV(id=show["id"])
         show_info_list.append(tv_obj.info())
 
     # until the user says stop, generates a random episode from a show
@@ -42,14 +42,22 @@ def main():
         result = get_random_episode(show_info_list)
         show = result[0]
         episode = result[1]
-        print('\nRANDOM EPISODE:',
-              '\nShow:', show['name'],
-              '\nSeason:', episode['season_number'],
-              '\nEpisode #:', episode['episode_number'],
-              '\nEpisode name:', episode['name'],
-              '\nDescription:', episode['overview'])
+        print(
+            "\nRANDOM EPISODE:",
+            "\nShow:",
+            show["name"],
+            "\nSeason:",
+            episode["season_number"],
+            "\nEpisode #:",
+            episode["episode_number"],
+            "\nEpisode name:",
+            episode["name"],
+            "\nDescription:",
+            episode["overview"],
+        )
 
-        go_again = input('\nTry again with the same shows? y/n: ') == 'y'
+        go_again = input("\nTry again with the same shows? y/n: ") == "y"
+
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -7,16 +7,10 @@ Conor Rafferty <craffer@umich.edu>
 from setuptools import setup
 
 setup(
-    name='randepisode',
-    version='0.1.0',
-    packages=['randepisode'],
+    name="randepisode",
+    version="0.1.0",
+    packages=["randepisode"],
     include_package_data=True,
-    install_requires=[
-        'tmdbsimple'
-    ],
-    entry_points={
-        'console_scripts': [
-            'randepisode = randepisode.__main__:main'
-        ]
-    },
+    install_requires=["tmdbsimple"],
+    entry_points={"console_scripts": ["randepisode = randepisode.__main__:main"]},
 )
