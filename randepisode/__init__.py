d94b88fdc9d4004ed9d53b918fd87bdfa564fdb8
Conor Rafferty <craffer@umich.edu>
2020-04-21 18:54:21 -0400

    Add flask config setup

--- randepisode/__init__.py
+++ randepisode/__init__.py
@@ -4,6 +4,9 @@ import flask
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 
+# Read settings from config module (randepisode/config.py)
+app.config.from_object("randepisode.config")
+
 # tell our app about our API and views files
 import insta485.api  # noqa: E402  pylint: disable=wrong-import-position
 import insta485.views  # noqa: E402  pylint: disable=wrong-import-position
