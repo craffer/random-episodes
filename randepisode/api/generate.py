5f4cf4e46bee5d3a568125316cc5e8d26b7536dc
Conor Rafferty <craffer@umich.edu>
2020-04-22 13:45:08 -0400

    Receive only IDs on the back end

--- randepisode/api/generate.py
+++ randepisode/api/generate.py
@@ -12,8 +12,7 @@ def generate_random_episode():
 
     if flask.request.method == "POST":
         # retrieve the list of show IDs from the data sent in the POST request
-        content = flask.request.get_json()
-        show_ids = content["ids"]
+        show_ids = flask.request.get_json()
         # select a show at random
         random_id = random.choice(show_ids)
 
