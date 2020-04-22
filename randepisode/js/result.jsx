643ca2aaf95ff1ab28fc281bd101ba755bd53a7a
Conor Rafferty <craffer@umich.edu>
2020-04-22 14:07:39 -0400

    Initial (VERY rough) display of result

--- randepisode/js/result.jsx
+++ randepisode/js/result.jsx
@@ -6,6 +6,19 @@ export default class Result extends React.Component {
   }
 
   render() {
-    return <div className="result"></div>;
+    if (this.props.visible) {
+      return (
+        <div className="result">
+          <h3>Random Episode:</h3>
+          <h4>
+            {this.props.show.name}: Season {this.props.episode.season_number},
+            Episode {this.props.episode.episode_number}
+          </h4>
+          <h3>{this.props.episode.name}</h3>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
