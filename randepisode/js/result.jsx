0a2aec64893b27621605b31ed9c93f9474fb618a
Conor Rafferty <craffer@umich.edu>
2020-04-22 12:21:16 -0400

    Fix typo

--- randepisode/js/result.jsx
+++ randepisode/js/result.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class Button extends React.Component {
+export default class Result extends React.Component {
   constructor(props) {
     super(props);
   }
