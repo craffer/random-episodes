a73b107c05786c4a4b2d7c1ee18e69b5df7e405e
Conor Rafferty <craffer@umich.edu>
2020-04-25 14:45:28 -0400

    Remove console.log statements

--- randepisode/js/searchbar.jsx
+++ randepisode/js/searchbar.jsx
@@ -39,7 +39,6 @@ export default class SearchBar extends React.Component {
   search() {
     if (this.state.query.length > 3) {
       const url = `api/v1/search?q=${encodeURI(this.state.query)}`;
-      console.log(url);
       fetch(url, { credentials: "same-origin" })
         .then((response) => {
           if (!response.ok) throw Error(response.statusText);
